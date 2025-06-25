57ccc42f288878f444962e621b4bf4cdface421a
Raúl Sebastián Cruz Baño <raul.cruz@udla.edu.ec>
2025-06-25 17:45:19 -0500

    Agregar archivos de proyecto.

---
+++ MiniCore/Models/Vendedor.cs
@@ -0,0 +1,10 @@
+﻿namespace MiniCore.Models
+{
+    public class Vendedor
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+
+        public ICollection<Venta> Ventas { get; set; }
+    }
+}
---
+++ MiniCore/Models/Venta.cs
@@ -0,0 +1,12 @@
+﻿namespace MiniCore.Models
+{
+    public class Venta
+    {
+        public int Id { get; set; }
+        public DateTime Fecha { get; set; }
+        public decimal Monto { get; set; }
+
+        public int VendedorId { get; set; }
+        public Vendedor Vendedor { get; set; }
+    }
+}
