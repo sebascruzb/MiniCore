533390c73fba3401eb1599259710ecc7d70a2d47
Raúl Sebastián Cruz Baño <raul.cruz@udla.edu.ec>
2025-06-25 17:46:55 -0500

    Creacion clase regla

---
+++ MiniCore/Models/Regla.cs
@@ -0,0 +1,10 @@
+﻿namespace MiniCore.Models
+{
+    public class Regla
+    {
+        public int Id { get; set; }
+        public decimal Minimo { get; set; }
+        public decimal Maximo { get; set; }
+        public decimal Porcentaje { get; set; }
+    }
+}
