d87840dffc962934ddd49d4db45344137c35da33
Raúl Sebastián Cruz Baño <raul.cruz@udla.edu.ec>
2025-06-25 17:58:36 -0500

    Creacion clase ComisionViewModel y AplicationDbContext

---
+++ MiniCore/Models/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace MiniCore.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options) { }
+
+        public DbSet<Vendedor> Vendedores { get; set; }
+        public DbSet<Venta> Ventas { get; set; }
+        public DbSet<Regla> Reglas { get; set; }
+    }
+}
---
+++ MiniCore/Models/ComisionViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace MiniCore.Models
+{
+    public class ComisionViewModel
+    {
+        public DateTime FechaInicio { get; set; }
+        public DateTime FechaFin { get; set; }
+
+        public List<Venta> Ventas { get; set; }
+        public Dictionary<int, decimal> ComisionesPorVendedor { get; set; }
+    }
+}
