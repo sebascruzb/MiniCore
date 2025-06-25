8b012312fbdbecbe103b78416609db8b29519a18
Raúl Sebastián Cruz Baño <raul.cruz@udla.edu.ec>
2025-06-25 18:28:52 -0500

    Cambio en program.cs

--- MiniCore/Models/ApplicationDbContext.cs
+++ MiniCore/Models/ApplicationDbContext.cs
@@ -6,7 +6,9 @@ namespace MiniCore.Models
     public class ApplicationDbContext : DbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-        : base(options) { }
+         : base(options)
+        {
+        }
 
         public DbSet<Vendedor> Vendedores { get; set; }
         public DbSet<Venta> Ventas { get; set; }
